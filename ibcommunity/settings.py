e89aa7ce2dfdd90381889769a709807da048c407
lucas <Lucazartu@gmail.com>
2017-10-27 16:00:31 -0300

    settings.py

--- ibcommunity/settings.py
+++ ibcommunity/settings.py
@@ -178,9 +178,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media Files
 MEDIA_URL = '/media/'
