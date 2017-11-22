8c6fbec3768f7c831ceb134b425a1693780c53f1
Lucas Artur <laqcs@cin.ufpe.br>
2017-11-22 14:26:46 -0300

    harvest period converted to new model

--- product/constants.py
+++ product/constants.py
@@ -1,4 +1,7 @@
 from django.utils.translation import ugettext_lazy as _
+from django.utils.dates import MONTHS
+
+
 PROVENANCE_TYPE = (
     (_("Natural"), _("Natural")),
     (_("Plantio"), _("Plantio")),
@@ -12,3 +15,11 @@ COLLECTION_TYPE = (
     (_("Praia"), _("Praia")),
     (_("Rio"), _("Rio")),
 )
+
+BS_CHOICES = (
+    (_("Patrimônio genético"), _("Patrimônio genético")),
+    (_("CTA identificável"), _("CTA identificável")),
+    (_("CTA não identificável"), _("CTA não identificável")),
+)
+
+MONTHS_CHOICES = [(MONTHS[i], MONTHS[i]) for i in MONTHS]
--- product/models.py
+++ product/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-from .constants import PROVENANCE_TYPE, COLLECTION_TYPE
+from .constants import PROVENANCE_TYPE, COLLECTION_TYPE, BS_CHOICES, MONTHS_CHOICES
 
 
 class ProductCollectionPoint(models.Model):
@@ -19,6 +19,21 @@ class ProductCollectionPoint(models.Model):
         return self.location_type
 
 
+class ProductHarvestPeriod(models.Model):
+    harvest_period = models.CharField(_("Período"), max_length=255,
+                                      blank=False, choices=MONTHS_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = _("Período da safra")
+        verbose_name_plural = _("Períodos de safra")
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.harvest_period
+
+
 class Product(models.Model):
     scientific_name = models.CharField(_("Nome científico"), max_length=255,
                                        blank=True, null=True)
@@ -29,17 +44,19 @@ class Product(models.Model):
     fruit_anual_volume = models.PositiveIntegerField(_("Voume anual do fruto"),
                                                      default=0, help_text=_("Em KG"))
     seed_anual_volume = models.PositiveIntegerField(_("Voume anual da semente"),
-                                                     default=0, help_text=_("Em KG"))
+                                                    default=0, help_text=_("Em KG"))
     pulp_anual_volume = models.PositiveIntegerField(_("Voume anual da polpa"),
-                                                     default=0, help_text=_("Em KG"))
-    harvest_period = models.TextField(_("Período da safra"), blank=True, null=True,
-                                      help_text=_("Informações sobre o período da safra"))
+                                                    default=0, help_text=_("Em KG"))
+    harvest_period = models.ManyToManyField(ProductHarvestPeriod, verbose_name=_("Período da safra"),
+                                            blank=True,
+                                            help_text=_("Informações sobre o período da safra"))
     certification_origin = models.TextField(_("Orgão da certificação"), blank=True, null=True,
                                             help_text=_("Informações sobre o orgão que expediu\
                                                         a certificação do produto."))
-    benefit_sharing_value = models.TextField(_("Regra de repartição de benefício"), blank=False, null=True)
+    benefit_sharing_value = models.CharField(_("Regra de repartição de benefício"), max_length=255,
+                                             blank=True, null=True, choices=BS_CHOICES)
     collection_point = models.ForeignKey(ProductCollectionPoint, verbose_name=_("Local de coleta/colheita"),
-                                            related_name='products_collect', blank=True, null=True)
+                                         related_name='products_collect', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
