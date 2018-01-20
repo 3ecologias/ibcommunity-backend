3bcfc24db9471e8b8c84fa1044d3ebab6a9d6d7a
Cleyton Fabio (pixies) <cleyton.flb@gmail.com>
2018-01-20 10:02:46 -0300

    admin file accounts app

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,14 +1,1 @@
 from django.contrib import admin
-from .models import Profile, Client
-
-
-class ProfileAdmin(admin.ModelAdmin):
-    icon = '<i class="material-icons">account_circle</i>'
-
-
-class ClientAdmin(admin.ModelAdmin):
-    icon = '<i class="material-icons">account_box</i>'
-
-
-admin.site.register(Profile, ProfileAdmin)
-admin.site.register(Client, ClientAdmin)
