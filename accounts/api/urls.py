8004fa721b80bac749eeee6a6c446af3385c3880
Cleyton Fabio (pixies) <cleyton.flb@gmail.com>
2018-01-24 09:42:47 -0300

    new reference for accounts api views

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -2,14 +2,15 @@ from django.conf.urls import url
 
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from accounts.views import UserList, UserDetail, UserCreate
+#from accounts.views import UserList, UserDetail, UserCreate
+from accounts.api.views import UserCreateAPIView, UserListAPIView, UserDetailAPIView
 
 urlpatterns = [
     # Users
     # path api/users/
-    url(r'^list/$', UserList.as_view(), name='client-list'),
-    url(r'^create/$', UserCreate.as_view(), name='client-create'),
-    url(r'^detail/(?P<pk>[0-9]+)/$', UserDetail.as_view(), name='client-detail')
+    url(r'^list/$', UserListAPIView.as_view(), name='client-list'),
+    url(r'^create/$', UserCreateAPIView.as_view(), name='client-create'),
+    url(r'^detail/(?P<pk>[0-9]+)/$', UserDetailAPIView.as_view(), name='client-detail')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
