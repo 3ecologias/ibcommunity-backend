b53dfc2e6f7cf54706ed7658f7d5e6cf6e196ed3
Cleyton Fabio (pixies) <cleyton.flb@gmail.com>
2018-01-24 09:33:19 -0300

    new urls for api clients

--- client/api/urls.py
+++ client/api/urls.py
@@ -3,8 +3,7 @@ from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from client.views import ProfileList, ProfileDetail, ProfileCreate,\
-                   ClientList, ClientDetail, ClientCreate,\
-                   UserList, UserDetail, UserCreate
+                   ClientList, ClientDetail, ClientCreate
 
 urlpatterns = [
     # Profile
@@ -16,11 +15,6 @@ urlpatterns = [
     url(r'^client/list/$', ClientList.as_view(), name='client-list'),
     url(r'^client/create/$', ClientCreate.as_view(), name='client-create'),
     url(r'^client/detail/(?P<pk>[0-9]+)/$', ClientDetail.as_view(), name='client-detail'),
-
-    # Users
-    url(r'^list/$', UserList.as_view(), name='client-list'),
-    url(r'^create/$', UserCreate.as_view(), name='client-create'),
-    url(r'^detail/(?P<pk>[0-9]+)/$', UserDetail.as_view(), name='client-detail')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
